0950cc9495dde9cdb83cae78d2c2fd50346677af
Akshay <akshaydighorikar@gmail.com>
2020-10-02 20:32:27 +0530

    Initial Commit

---
+++ precache-manifest.a63c0ae7007e66918caffb03c91f6207.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28171c73547df47f72e3a7b14b0bbc81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd2ad1342ec8381c92d2",
+    "url": "/static/css/10.34917357.chunk.css"
+  },
+  {
+    "revision": "4f474163a3bc1c7cccb0",
+    "url": "/static/css/5.6a7243f3.chunk.css"
+  },
+  {
+    "revision": "4084e431be90f01d281e",
+    "url": "/static/css/6.36fb357c.chunk.css"
+  },
+  {
+    "revision": "d7c3c14863b5f3e3b530",
+    "url": "/static/css/7.f5efd949.chunk.css"
+  },
+  {
+    "revision": "7d23e3154823feadc3a5",
+    "url": "/static/css/8.2c28e1a7.chunk.css"
+  },
+  {
+    "revision": "1cd3ada0c8b08c2d8b36",
+    "url": "/static/css/9.2beb8d03.chunk.css"
+  },
+  {
+    "revision": "a45902a060d920d9e07d",
+    "url": "/static/css/main.dc99cce0.chunk.css"
+  },
+  {
+    "revision": "4c65f2d0a464aca1046f",
+    "url": "/static/js/0.d10b7835.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/static/js/0.d10b7835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2ad1342ec8381c92d2",
+    "url": "/static/js/10.01ddf6ae.chunk.js"
+  },
+  {
+    "revision": "8d9b8056fe8e439bc3dd",
+    "url": "/static/js/11.8ab992c8.chunk.js"
+  },
+  {
+    "revision": "877dedb269429f809a08",
+    "url": "/static/js/3.54dc70c9.chunk.js"
+  },
+  {
+    "revision": "2f1704898b754a25fd9d3b781528bef3",
+    "url": "/static/js/3.54dc70c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b015388c7717da90b388",
+    "url": "/static/js/4.8dd7cfc8.chunk.js"
+  },
+  {
+    "revision": "4f474163a3bc1c7cccb0",
+    "url": "/static/js/5.a7d78f35.chunk.js"
+  },
+  {
+    "revision": "4084e431be90f01d281e",
+    "url": "/static/js/6.a925ec22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/6.a925ec22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7c3c14863b5f3e3b530",
+    "url": "/static/js/7.b285786f.chunk.js"
+  },
+  {
+    "revision": "7d23e3154823feadc3a5",
+    "url": "/static/js/8.a72f2d6b.chunk.js"
+  },
+  {
+    "revision": "1cd3ada0c8b08c2d8b36",
+    "url": "/static/js/9.887861dd.chunk.js"
+  },
+  {
+    "revision": "a45902a060d920d9e07d",
+    "url": "/static/js/main.95ae1485.chunk.js"
+  },
+  {
+    "revision": "f7fae1d8c8ee67242c3d",
+    "url": "/static/js/runtime-main.53bf8fd1.js"
+  },
+  {
+    "revision": "9c9f09dcd8320e547b92001fa905d80f",
+    "url": "/static/media/Akshay_Dighorikar_CV.9c9f09dc.pdf"
+  },
+  {
+    "revision": "53c09badc53e96dd89dec2914b5a96d1",
+    "url": "/static/media/Icon.53c09bad.svg"
+  },
+  {
+    "revision": "11ff8774d3332bce75887a63ba502519",
+    "url": "/static/media/about-img.11ff8774.webp"
+  },
+  {
+    "revision": "d4c1dde7be1878fa0430bd987638000d",
+    "url": "/static/media/avatar.d4c1dde7.webp"
+  },
+  {
+    "revision": "74efc23c3fb1a471c47e1ada0f3ef29b",
+    "url": "/static/media/bg.74efc23c.webp"
+  },
+  {
+    "revision": "eb9cda419f8bbb198b6b82b46b01ed8d",
+    "url": "/static/media/ecommerce.eb9cda41.webp"
+  },
+  {
+    "revision": "0888359fdaae30d50ae9929c7f42db59",
+    "url": "/static/media/express_traker.0888359f.webp"
+  },
+  {
+    "revision": "fd81777ef4490846e20e278139708d5b",
+    "url": "/static/media/home_img.fd81777e.svg"
+  },
+  {
+    "revision": "c233755cdd8681c8517d7d8de597d397",
+    "url": "/static/media/mailbox.c233755c.svg"
+  }
+]);
